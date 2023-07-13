ed261ecb70218dea8cfef6c63d8edaf8b3bbf710
GerardoPuertosSada <gerardopsada@gmail.com>
2023-07-12 19:22:58 -0600

    user schema done

--- backend/models/tareasModel.js
+++ backend/models/tareasModel.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
 const tareaSchema = mongoose.Schema({
+  user:{
+      type: 
+  },
   texto: {
       type: String,
       required: [true, 'por favor teclea una descripción a la tarea']
