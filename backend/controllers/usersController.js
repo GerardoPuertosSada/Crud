439361da8ff58b3b80570f156406ea35ed7b4ad9
GerardoPuertosSada <gerardopsada@gmail.com>
2023-07-12 21:16:51 -0600

    jwt

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -60,7 +60,8 @@ const loginUser = asyncHandler( async (req, res) =>{
     res.json({
       _id: user._id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
@@ -72,6 +73,12 @@ const loginUser = asyncHandler( async (req, res) =>{
 const getUserData = asyncHandler( async (req, res) =>{
     res.json({message: 'get user data'})
 })
+
+const generateToken = (id) =>{
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+}
 
 module.exports = {registerUser, loginUser, getUserData}
 
